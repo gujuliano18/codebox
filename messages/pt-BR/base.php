a19e356101774f60fa56c9a1e085d3e647fa27c1
Juliano de Souza <gujuliano18@gmail.com>
2024-10-11 21:24:15 -0300

    Add files via upload
    Adicionado diretório de traduções com idioma português do brasil, duas alterações feitas em update.php e create.php em codebox/views/admin para acomodar um trecho da tradução. detalhes descristos no base.php na pasta pt-BR.
    
    Signed-off-by: Juliano de Souza <gujuliano18@gmail.com>

---
+++ messages/pt-BR/base.php
@@ -0,0 +1,25 @@
+<?php
+return array (
+//models/ConfigureForm.php
+  'ID' => 'Salvar como favorito',
+  'Title:' => 'Título:',
+  'Codebox HTML code snippet:' => 'Trecho de código HTML na Codebox:',
+  'Sort Order' => 'Ordem de Classificação',
+//Global
+  'Update' => 'Atualizar',
+  'Delete' => 'Apagar',
+//Events.php
+  'Codebox Settings' => 'Ajustes da Codebox',
+//views/admin/_view.php
+  'Are you sure you want to delete this item?' => 'Tem certeza de que deseja excluir este item?',
+//views/admin/index.php
+  '<strong>Codebox</strong> module configuration' => 'Configuração do módulo <strong>Codebox</strong>',
+  'No entries found.' => 'Nenhuma entrada encontrada.',
+  'Add Entry' => 'Adicionar',
+ //views/admin/update.php 
+  '<strong>Update</strong>' => '<strong>Atualização</strong>',
+  'Title' => 'Título', //placehold compartilhado por update.php e create.php - linha original modificada pode ser alterada caso o módulo seja atualizado 
+//views/admin/create.php
+  '<strong>Add New Entry</strong>' => '<strong>Adicionar Novo</strong>',
+  'Save' => 'Salvar',
+);
